f7daaf7f022a3a4e800ef15c87678e847e537952
bdalberson <Brian.alberson@gmail.com>
2023-04-19 10:05:13 -0700

    getting file writing

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+
+
 title = process.argv[2]
 desc = process.argv[3]
 toC = process.argv[4]
@@ -9,3 +13,16 @@ questions = process.argv[9]
 license = process.argv[10]
 username = process.argv[11]
 email = process.argv[12]
+
+
+fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) => {
+    // Ternary operator takes in a condition followed by a question mark (?)
+    // then an expression to execute if the condition is truthy followed by a colon (:)
+    // and finally the expression to execute if the condition is falsy.
+    // This operator is frequently used as a shortcut for the if statement.
+    err ? console.error(err) : console.log('Commit logged!')
+  
+      fs.readFile('log.txt','utf8', (error, data) => {
+        error ? console.error(error) : console.log(data)
+      });
+  });
